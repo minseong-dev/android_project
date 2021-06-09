8a28c779c76200dcb6cbc59eda9c61971f91d785
minseong-dev <aa971020@gmail.com>
2021-06-09 15:54:42 +0900

    chatting추가

--- node/api/controller/farm.js
+++ node/api/controller/farm.js
@@ -120,20 +120,43 @@ class farm {
 
             let chat_id = 1000000 * Math.random();
             const zone_id = req.body.zone_id;
-            const date_sent = req.body.date_sent;
             const type_sent = req.body.type_sent;
             const chat_type = req.body.chat_type;
             const chat_text = req.body.chat_text;
             const chat_img = req.body.chat_img;
+            const date_sent = req.body.date_sent;
 
             if (chat_type == 'text') {
-                await db('insert into chat(id, date_sent, chat_text, type_sent, zone_id, chat_type) values(?, ?, ?, ?, ?, ?)', [chat_id, date_sent, chat_text, type_sent, zone_id, chat_type])
+                await db('insert into chat(id, date_sent, chat_text, type_sent, zone_id, chat_type) values(?, ?, ?, ?, ?, ?)', 
+                [chat_id, date_sent, chat_text, type_sent, zone_id, chat_type])
             } 
             else if (chat_type == 'img') {
-                await db('insert into chat(id, date_sent, chat_img, type_sent, zone_id, chat_type) values(?, ?, ?, ?, ?, ?)', [chat_id, date_sent, chat_img, type_sent, zone_id, chat_type])
+                await db('insert into chat(id, date_sent, chat_img, type_sent, zone_id, chat_type) values(?, ?, ?, ?, ?, ?)', 
+                [chat_id, date_sent, chat_img, type_sent, zone_id, chat_type])
             }
         
             res.status(httpStatus.OK).send('채팅등록완료')
+
+        }
+        
+        catch (error) {
+            console.error(error)
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send([])
+        }
+    }
+
+    async chatting(req, res) {
+
+        try{
+
+            const zone_id = req.query.zone_id;
+
+            let chatInfo = await db('select chat_text, chat_img, type_sent from chat where zone_id=? order by date_sent', [zone_id])
+            const mainInfo = {
+                chatInfo : chatInfo
+            }
+            
+            res.status(httpStatus.OK).send(mainInfo)
 
         }
         
